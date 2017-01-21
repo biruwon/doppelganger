742fd54d1a87de8b7429f7fe652b86f9b5ee5207
Antonio Jesus <ajesus.etsii@gmail.com>
2017-01-21 12:22:11 +0100

    Added googleimages spider

--- doppelganger/settings.py
+++ doppelganger/settings.py
@@ -17,9 +17,10 @@ NEWSPIDER_MODULE = 'doppelganger.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'doppelganger (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:7.0.1) Gecko/20100101 Firefox/7.7'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+#ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,7 +28,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.5
+DOWNLOAD_DELAY = 7
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -68,8 +69,9 @@ ITEM_PIPELINES = {
     'doppelganger.pipelines.DoppelgangerPipeline': 1,
 }
 
-#IMAGES_STORE = '/home/arodriguez/tsp/projects/personal/doppelganger/images'
-IMAGES_STORE = '/home/arodriguez/tsp/projects/personal/doppelganger/images_test'
+#IMAGES_STORE = '/home/arodriguez/tsp/projects/personal/doppelganger/images/adultfimdatabase'
+#IMAGES_STORE = '/home/arodriguez/tsp/projects/personal/doppelganger/bestpornstartdb'
+IMAGES_STORE = '/home/arodriguez/tsp/projects/personal/doppelganger/images/google_images'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
---
+++ doppelganger/spiders/googleimages.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import string
+import re
+import os
+import json
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy import Request
+from doppelganger.items import Actress
+from scrapy.shell import inspect_response
+
+
+class GoogleImagesSpider(CrawlSpider):
+
+    name = "googleimages"
+    allowed_domains = ["google.com"]
+
+    start_urls = []
+    for directory_name in os.listdir("./images/bestpornstartdb/full/"):
+    #for directory_name in os.listdir("./images/test/full/"):
+        actress_name = directory_name.replace('_', ' ')
+        # google_url = 'https://www.google.com/search?q=' + actress_name + ' porn' + '&biw=1536&bih=776&tbm=isch&tbs=isz:m,itp:face&tbm=isch'
+        google_url = 'https://www.google.com/search?q=' + actress_name + ' porn' + '&biw=1536&bih=776&tbm=isch&tbs=isz:m&tbm=isch' # not faces
+        start_urls.append(google_url)
+
+    #start_urls = ['file:///home/arodriguez/tsp/projects/personal/doppelganger/tmp8n_6z1.html']
+
+    def parse(self, response):
+
+        item = Actress()
+
+        value_searched = response.xpath('//input[@id="lst-ib"]/@value').extract()[0]
+        actress_name = value_searched.replace('""', '').replace(' ', '_').replace('_porn', '')
+        item['name'] = actress_name
+
+        item['image_urls'] = []
+        actress_info_list = response.xpath('//div[@class="rg_meta"]/text()').extract()
+        for actress_info in actress_info_list[:40]:
+
+            picture_info_dic = json.loads(actress_info)
+
+            item['image_urls'].append(picture_info_dic['tu'])
+
+        return item
